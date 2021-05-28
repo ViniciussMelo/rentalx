249ea7bf4db5944a4e07bf880943ed7945c9479b
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2021-05-28 11:27:38 -0300

    feat: add categories integration test

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,10 +2,12 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+  const isTest = process.env.NODE_ENV === "test";
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: isTest ? "localhost" : host,
+      database: isTest ? "rentalx_test" : defaultOptions.database,
     })
   );
 };
