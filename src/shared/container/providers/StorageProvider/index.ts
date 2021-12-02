a0b508de0f2aa7e34427d3b270bb7cc2580ab585
ViniciussMelo <vinicius.smelo54@gmail.com>
2021-12-02 16:44:58 -0300

    feat: add S3 storage provider

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,9 +1,16 @@
+import "dotenv/config";
 import { container } from "tsyringe";
 
+import { S3StorageProvider } from "./implementations/S3StorageProvider";
 import { LocalStorageProvider } from "./in-memory/LocalStorageProvider";
 import { IStorageProvider } from "./IStorageProvider";
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  diskStorage[process.env.disk]
 );
