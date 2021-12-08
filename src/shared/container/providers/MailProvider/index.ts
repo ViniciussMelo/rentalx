fb8432bebdd92aba3c6fe5d75f5f7f1ee7ae6e73
ViniciussMelo <vinicius.smelo54@gmail.com>
2021-12-08 15:16:42 -0300

    feat: add SES mail provider

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -1,9 +1,16 @@
+import "dotenv/config";
 import { container } from "tsyringe";
 
 import { IMailProvider } from "./IMailProvider";
 import { EtherealMailProvider } from "./implementations/EtherealMailProvider";
+import { SESMailProvider } from "./implementations/SESMailProvider";
+
+const mailProvider = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
 
 container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  new EtherealMailProvider()
+  "MailProvider",
+  mailProvider[process.env.MAIL_PROVIDER]
 );
